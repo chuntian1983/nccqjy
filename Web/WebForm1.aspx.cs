8f51d19fb781e1af889556e9588066bb8f2ff49e
春天 <chuntian1983@163.com>
2017-09-19 14:01:29 +0800

    2017-9-19

--- Web/WebForm1.aspx.cs
+++ Web/WebForm1.aspx.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Maticsoft.DBUtility;
+using System.Data;
 
 namespace Web
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        DataTable dt = new DataTable();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            for (int i = 0; i < 100; i++)
+            {
+                dt = DbHelperSQL.Query("select * from t_bid").Tables[0];
+                Response.Write(i.ToString()+"<br>");
+            }
         }
     }
 }
